294833448c2e6b6b0aef3ed673f587da75547847
martindwyer <martin@mdbytes.com>
2020-12-22 05:08:31 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,12 +4,15 @@ const app = express()
 const publicPath = path.join(__dirname, '..', 'public')
 const port = process.env.PORT || 3000
 
+console.log('setting up public path')
 app.use(express.static(publicPath))
 
+console.log('setting up default path')
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'))
 })
 
+console.log('telling app to listen on port')
 app.listen(port, () => {
   console.log('Server is up!')
 })
